1c4a390f6d4a41bad4a113c06f0925c5643b7c16
Jasper Alani <jalani@torpedogroup.com>
2022-10-23 18:17:20 +0100

    Fix multiple minor issues and update readme

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 ## alexandria
-<img class="badge" tag="github.com/alexandria-api/go" src="https://goreportcard.com/badge/github.com/alexandria-api/go">
+[<img class="badge" tag="github.com/alexandria-api/go" src="https://goreportcard.com/badge/github.com/alexandria-api/go">](https://goreportcard.com/badge/github.com/alexandria-api/go)
 
 #### version 1.0.2
 GO Image Storage and Retrieval API using [Gin](https://github.com/gin-gonic/gin)
 
-Made with images in mind, but I guess aside from compression it can handle most if not all file types. I have not tested this.
-### Requirements
-[Go](https://www.php.net/), [imagecomp](https://github.com/aprimadi/imagecomp)
+Made with images in mind, but I guess aside from compression it can handle most if not all file types.
 
-### Setup
-1. Edit config.go with your preferred allowed file extensions (Only png and jpg supported for compression).
-2. Enter src dir
-3. Run `go build -o alexandria`
-2. Run `./alexandria`
+### Software Requirements
+- [Go](https://go.dev/)
+- [imagecomp](https://github.com/aprimadi/imagecomp)
+
+### Go Dependancies
+- `go get github.com/gin-gonic/gin`
+- `go get github.com/rs/xid`
+
+### Run
+- `go run ./`
+- `go build -o alexandria && ./alexandria`
 
 ### Todo:
 - ~~Upload~~
@@ -22,17 +26,18 @@ Made with images in mind, but I guess aside from compression it can handle most 
 - ~~Check if file is png or jpg for compression~~
 - ~~Add uploads to queue, so they can be compressed while the http request is finished~~
 - ~~Implement a proper job queue~~
-- Refactor largest functions
-- Function that processes job queue on startup
 - ~~Allow a limit to be put on the amount of simultaneous image compressions.~~
+- Tests
+- Function that processes job queue on startup
 - Add logging to all errors
+- Upload multiple files at once
 - Check if image already exists in storage
   - By filename
   - Using ImageMagick
-- Tests
 - Allow for optional compression
 - Rate limit
 - Add support for other file types
 - Process temporary folder into queue on start up
 - Add metadata signalling when image has been compressed
 - Process queue folder by checking metadata on start up
+- Add clear state command
---
+++ actions.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Upload action
+// Upload a file into a compression queue or storage.
+func Upload(c *gin.Context) {
+
+	// Validate upload
+
+	file, err := c.FormFile("file")
+	if nil != err {
+		respond(c, 400, gin.H{
+			"error":   "bad request",
+			"message": "A file must be supplied as value in form data with key 'file'",
+		})
+		return
+	}
+
+	// file.Filename must be a file name and an extension
+	matched, err := regexp.MatchString(`[^\\]*\.(\w+)$`, file.Filename)
+	if nil != err {
+		respond(c, 400, gin.H{
+			"error":   "bad request",
+			"message": "File name must be a file name and an extension",
+		})
+		return
+	}
+
+	if !matched {
+		respond(c, 400, gin.H{
+			"error":   "bad request",
+			"message": "Content Disposition Filename must be supplied",
+		})
+		return
+	}
+
+	var (
+		explodedFilename []string = strings.Split(file.Filename, ".")
+		extension                 = explodedFilename[1]
+	)
+
+	if !stringInSlice(strings.ToLower(extension), config.permittedFileExtensions) {
+		var pretty = strings.Join(config.permittedFileExtensions, ", ")
+		respond(c, 400, gin.H{
+			"error":   "bad request",
+			"message": "File extension must be one of: " + pretty,
+		})
+		return
+	}
+
+	var (
+		filename       = generateIdentifier()
+		imageID        = &imageIdentifier{id: filename}
+		fullname       = "/" + filename + "." + extension
+		queuePath      = config.dir.queue + fullname
+		successfulPath = config.dir.images + fullname
+	)
+
+	// Move image to queue location
+
+	err = c.SaveUploadedFile(file, queuePath)
+	if nil != err {
+		respond(c, 500, gin.H{
+			"error":   "server failure",
+			"message": "Failed to save image to queue location.",
+		})
+		return
+	}
+	imageID.updateState("queue")
+
+	var compressableExtensions = []string{"png", "jpg", "jpeg"}
+
+	if contains(compressableExtensions, strings.ToLower(extension)) {
+
+		// Move to backlog if queue is full
+
+		go imageID.compressAndFinish(queuePath, successfulPath, false)
+		positionInQueue++
+		respond(c, http.StatusOK, gin.H{
+			"success": "file added to queue",
+			"message": "Position in queue: " + fmt.Sprintf("%v", positionInQueue),
+			"id":      filename,
+		})
+		log.Println("File added to compression queue.")
+		return
+	}
+
+	imageMoved := moveImage(queuePath, successfulPath)
+	if !imageMoved {
+		respond(c, 500, gin.H{
+			"error":   "server failure",
+			"message": "Failed to move image into storage location.",
+		})
+		return
+	}
+	imageID.updateState("finished")
+
+	respond(c, http.StatusOK, gin.H{
+		"success": "file uploaded",
+		"message": "File has been uploaded successfully.",
+		"id":      filename,
+	})
+	log.Println("File skipped compression and uploaded.")
+}
+
+// Retrieve action
+// Retrieve an uploaded file
+func Retrieve(c *gin.Context) {
+	id := c.Param("id")
+	if !validIdentifier(id) {
+		respond(c, 400, gin.H{
+			"error":   "bad request",
+			"message": "Invalid image identifier",
+		})
+		return
+	}
+
+	var (
+		root  = "storage/images"
+		files []string
+	)
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	var foundFile = "not found"
+
+	for _, file := range files {
+		if strings.Contains(file, id) {
+			foundFile = file
+		}
+	}
+
+	if "not found" == foundFile {
+		respond(c, http.StatusOK, gin.H{
+			"error":   "file not found",
+			"message": "No file matching the supplied identifier was found.",
+		})
+		return
+	}
+
+	c.File(foundFile)
+}
